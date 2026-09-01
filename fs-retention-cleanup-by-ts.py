cc32318d28a361b21b167e5d478e95fb10c61e97
Kleber Felix <45995918+kgfelix@users.noreply.github.com>
2026-09-01 12:41:13 -0300

    Refactor file deletion logic and update cutoff date

--- fs-retention-cleanup-by-ts.py
+++ fs-retention-cleanup-by-ts.py
@@ -1,11 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Module Name: fs-retention-cleanup-by-ts.py
+Description: Deletes files from a specific filesystem folder that are older than the configured year.
+Author: Kleber Felix
+Version: 1.0.0
+Date: 2026-08-27
+How to use: python3 fs-retention-cleanup-by-ts.py <folder_path>
+"""
+
 import os
 import sys
 from datetime import datetime
 
-MAX_INTERACTIONS = 100
-MAX_ENTRIES = 100
-
-CUTOFF_TIMESTAMP = datetime(2025, 1, 1).timestamp()
+# Files modified before April 1, 2026 will be deleted
+CUTOFF_TIMESTAMP = datetime(2026, 4, 1).timestamp()
 
 if len(sys.argv) >= 2:
     file_path_arg = sys.argv[1]
@@ -17,31 +27,19 @@ else:
 with os.scandir(file_path_arg) as entries:
     all_files = [entry for entry in entries if entry.is_file()]
 
-count_interactions = 0
-index = 0
 count_deleted_files = 0
 
-while count_interactions < MAX_INTERACTIONS and index < len(all_files):
-    count_entries = 0
+for entry in all_files:
+    stats = entry.stat()
+    file_mtime = stats.st_mtime
+    readable_time = datetime.fromtimestamp(file_mtime).strftime('%Y-%m-%d %H:%M:%S')
 
-    while count_entries < MAX_ENTRIES and index < len(all_files):
-        entry = all_files[index]
-        stats = entry.stat()
-        file_mtime = stats.st_mtime
-        readable_time = datetime.fromtimestamp(file_mtime).strftime('%Y-%m-%d %H:%M:%S')
-
-        if file_mtime < CUTOFF_TIMESTAMP:
-            file_path = os.path.join(file_path_arg, entry.name)
-            print(f"{entry.name} | Removing (Modified: {readable_time})")
-            os.remove(file_path)
-            count_deleted_files +- 1
-        else:
-            print(f"{entry.name} | Keeping (Modified: {readable_time})")
-
-        count_entries += 1
-        index += 1
-
-    print("-------------------")
-    count_interactions += 1
+    if file_mtime < CUTOFF_TIMESTAMP:
+        file_path = os.path.join(file_path_arg, entry.name)
+        print(f"{entry.name} | Removing (Modified: {readable_time})")
+        os.remove(file_path)
+        count_deleted_files += 1
+    else:
+        print(f"{entry.name} | Keeping (Modified: {readable_time})")
 
 print(f"{count_deleted_files} files were deleted")
